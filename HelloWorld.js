69b400b8809cd7e23b3d4bd4c37b830beb901367
Hussain Ibrahim <hussain.ibrahim.530@gmail.com>
2017-06-17 21:54:28 +0500

    Hello world app first commit

---
+++ HelloWorld.js
@@ -0,0 +1,3 @@
+var unsme = process.argv[2];
+var message = "Hello World fro My first Project";
+console.log(message);
---
+++ HelloWorld.ts
@@ -0,0 +1,2 @@
+let message: string = "Hello World fro My first Project";
+console.log(message);
